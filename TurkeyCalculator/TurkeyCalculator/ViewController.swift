899f52725cb7fdbfd535658237a5e78ceb1bb8cd
Matt Martindale <martindalematt18@yahoo.com>
2020-02-13 19:14:31 -0700

    github acting weird

--- TurkeyCalculator/TurkeyCalculator/ViewController.swift
+++ TurkeyCalculator/TurkeyCalculator/ViewController.swift
@@ -19,12 +19,45 @@ class ViewController: UIViewController {
     // Actions
     
     @IBAction func calculateButtonPressed(_ sender: Any) {
+        guard let turkeyWeightString =
+            turkeyWeightTextField.text else { return }
         
+        guard let turkeyWeight = Double(turkeyWeightString) else {
+            print("Ivalid number")
+            return
+        }
+        
+        var durationInMinutes: Double
+        
+        if metricButton.isSelected {
+            durationInMinutes = cookTimeInKilograms(turkeyWeight)
+        } else {
+            durationInMinutes = cookTimeInPounds(turkeyWeight)
+        }
+
+        cookTimeTextField.text = "\(durationInMinutes)"
     }
     
     @IBAction func metricButtonPressed(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
         
+        button.isSelected.toggle()
     }
     
     // Helper functions
+    func cookTimeInPounds(_ weight: Double) -> Double{
+        let durationInMinutes = weight * 15
+        return durationInMinutes
+    }
+    
+    func cookTimeInKilograms(_ weight: Double) -> Double {
+        var durationInMinutes: Double
+        
+        if weight >= 4 {
+            durationInMinutes = 20 * weight + 90
+        } else {
+            durationInMinutes = 20 + weight + 70
+        }
+        return durationInMinutes
+    }
 }
